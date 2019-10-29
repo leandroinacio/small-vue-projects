f14498cc3cbfac434ff8d1732843db3b06629b1c
Leandro Souza <le.inacio.souza@gmail.com>
2019-10-28 17:28:11 -0700

    Working on About and Signup pages

--- mevn-blog/src/views/About.vue
+++ mevn-blog/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="row">
-    <h1>ABOUT</h1>
+  <div class="row justify-content-center">
+    <div class="card">
+      <div class="card-header">Leandro Inácio Lima de Souza</div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <p>Web applications developer. Vue, in this case 😎</p>
+        </blockquote>
+      </div>
+    </div>
   </div>
 </template>
--- mevn-blog/src/views/Signup.vue
+++ mevn-blog/src/views/Signup.vue
@@ -1,11 +1,92 @@
 <template>
-  <div class="row">
-    SIGNUP
+  <div class="row justify-content-center">
+    <div class="col-6">
+      <div class="alert" v-bind:class="[alertStatus]" role="alert">
+        {{ alertMessage }}
+      </div>
+      <form>
+        <div class="form-row">
+          <div class="form-group col-md-12">
+            <label for="inputEmail">Email</label>
+            <input
+              v-model="email"
+              type="email"
+              class="form-control"
+              id="inputEmail"
+              placeholder="Email"
+            />
+          </div>
+          <div class="form-group col-md-12">
+            <label for="inputPassword">Password</label>
+            <input
+              type="password"
+              v-model="password"
+              class="form-control"
+              id="inputPassword"
+              placeholder="Password"
+            />
+          </div>
+          <div class="form-group col-md-12">
+            <label for="confirmPassword">Confirm Password</label>
+            <input
+              v-model="passwordConfirmation"
+              type="password"
+              class="form-control"
+              id="confirmPassword"
+              placeholder="Confirm Password"
+            />
+            <small v-show="!isPasswordMatch"
+              >Passwords must be at least 6 characters and confirmation must
+              match.</small
+            >
+          </div>
+        </div>
+        <button
+          @click.prevent="submitForm()"
+          type="submit"
+          class="btn btn-primary"
+        >
+          Register
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  data: () => {
+    return {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      alertStatus: "hidden",
+      alertMessage: ""
+    };
+  },
+  computed: {
+    isPasswordMatch() {
+      return (
+        this.password.length > 6 && this.password === this.passwordConfirmation
+      );
+    }
+  },
+  methods: {
+    submitForm() {
+      if (
+        this.email &&
+        this.password &&
+        this.passwordConfirmation &&
+        this.isPasswordMatch
+      ) {
+        this.alertStatus = "alert-sucess";
+        this.alertMessage = "User created with success";
+      } else {
+        this.alertStatus = "alert-danger";
+        this.alertMessage = "All fields are required";
+      }
+    }
+  }
 };
 </script>
